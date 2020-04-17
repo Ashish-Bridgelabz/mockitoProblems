3dc4cce96f2b489395d7769330662a1832379f17
Ashish-Bridgelabz <asaschoudhary49@gmail.com>
2020-04-17 23:09:13 +0530

    ADDED:Returned customer fullname instead of hard-coded value

---
+++ main/java/mockito/Customer.java
@@ -0,0 +1,12 @@
+package mockito;
+
+public class Customer {
+
+    public String FirstName;
+    public String LastName;
+
+    public Customer(String FirstName, String LastName) {
+        this.FirstName = FirstName;
+        this.LastName = LastName;
+    }
+}
